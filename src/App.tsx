abdb85dcf51b2adfec68aa0d5d4a05216190f321
CapSap <cshotam@gmail.com>
2023-01-25 10:42:55 +1100

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,6 @@ const App = () => {
   return <div>app hello</div>;
 };
 
-const container = document.getElementById("root")!;
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
