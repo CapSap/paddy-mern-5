1fee1e7c3dd42475004410fa52a8e0c4e929f3a1
CapSap <cshotam@gmail.com>
2023-02-06 14:22:42 +1100

    render entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import Entry from "./EntryPage";
 
 const App = () => {
-  return <div>app hello</div>;
+  return (
+    <div>
+      app hello
+      <Entry />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 
-// if (!container) {
-//   throw new Error("no container");
-// }
+if (!container) {
+  throw new Error("no container");
+}
 
 const root = createRoot(container);
 root.render(<App />);
