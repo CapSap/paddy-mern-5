bd78892791551933e7f417daa86ed6c39b1ec4d6
CapSap <cshotam@gmail.com>
2023-06-04 21:14:29 +1000

    use mongoose to connect to mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,17 @@ dotenv.config();
 const app: Express = express();
 const port = 3000;
 
+async function mongoConnect() {
+  if (!process.env.MONGO_URI) {
+    throw new Error("no uri");
+  }
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("db connected"))
+    .catch((err) => console.log(err));
+}
+mongoConnect();
+
 // app.use(cors());
 // enable cors (cross origin response)
 app.use(express.json());
@@ -21,32 +32,6 @@ app.use(morgan("dev"));
 // logging middleware
 
 app.use("/orders", router);
-
-const uri = `mongodb+srv://admin:${process.env.PASS}@cluster0.1ftxsem.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
