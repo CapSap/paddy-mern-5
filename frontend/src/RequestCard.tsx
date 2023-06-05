07ed67756e745f1d5398f7c6dfb52e3c10ebdb23
CapSap <cshotam@gmail.com>
2023-06-05 21:57:43 +1000

    card basics

--- frontend/src/RequestCard.tsx
+++ frontend/src/RequestCard.tsx
@@ -1,10 +1,11 @@
 export const RequestCard = ({ request }) => {
-  console.log(request);
   return (
-    <div className="border-cyan-100 border-8">
-      <p>Order Number / reference {request.orderNumber}</p>
-      <p>name {request.customerName}</p>
-      <p>Items {request.orderedItems[0].items}</p>
+    <div className="border-cyan-100 border-8 m-3">
+      <p>Order Number / reference: {request.orderNumber}</p>
+      <p>name: {request.customerName}</p>
+      <p>Items: {request.orderedItems[0].items}</p>
+      <p>Message: {request.orderedItems[0].message}</p>
+      <p>Destination Store: {request.pickupLocation}</p>
     </div>
   );
 };
