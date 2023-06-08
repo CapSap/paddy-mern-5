7f43a9faaa931b7241138363f2000f53c6cdb77f
CapSap <cshotam@gmail.com>
2023-06-08 15:14:25 +1000

    added type checking on the props for requestcard and a tiny bit of styling

--- frontend/src/RequestCard.tsx
+++ frontend/src/RequestCard.tsx
@@ -1,11 +1,38 @@
-export const RequestCard = ({ request }) => {
+import { OrderInfoFromDB } from "./Types";
+
+export const RequestCard = ({
+  order,
+  id,
+}: {
+  order: OrderInfoFromDB;
+  id: string;
+}) => {
   return (
-    <div className="border-cyan-100 border-8 m-3">
-      <p>Order Number / reference: {request.orderNumber}</p>
-      <p>name: {request.customerName}</p>
-      <p>Items: {request.orderedItems[0].items}</p>
-      <p>Message: {request.orderedItems[0].message}</p>
-      <p>Destination Store: {request.pickupLocation}</p>
+    <div
+      className="flex basis-96 flex-shrink-0 grow border-cyan-100 border-8 m-3 p-3"
+      id={id}
+    >
+      <div>
+        <p>Order Number / reference: {order.orderNumber}</p>
+        <p>name: {order.customerName}</p>
+        {order.orderedItems[0].message ? (
+          <p>Message: {order.orderedItems[0].message}</p>
+        ) : null}
+        <p className="font-bold">Destination Store: {order.pickupLocation}</p>
+        <p>Status: {order.orderedItems[0].requestStatus}</p>
+
+        <a href="#">Link to magento order</a>
+      </div>
+      <div className="flex-grow  text-center">
+        {order.orderedItems.map((request) => {
+          return (
+            <div>
+              <p>{request.items}</p>
+            </div>
+          );
+        })}
+        <p className="font-bold">Items: {order.orderedItems[0].items}</p>
+      </div>
     </div>
   );
 };
