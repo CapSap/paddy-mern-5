3d318ad84883bd626481267ad36142a974d05329
CapSap <cshotam@gmail.com>
2023-07-03 17:26:14 +1000

    add notes textarea and state

--- frontend/src/components/RequestCard.tsx
+++ frontend/src/components/RequestCard.tsx
@@ -22,6 +22,8 @@ export const RequestCard = ({
       ?.requestStatus
   );
 
+  const [requestNotes, setRequestNotes] = useState();
+
   if (!order) {
     return <div>no order</div>;
   }
@@ -159,6 +161,21 @@ export const RequestCard = ({
                     className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
                   />
                   {ibt && isNaN(+ibt) ? <p>Please enter numbers only</p> : null}
+                </div>
+                <div className="">
+                  <label
+                    htmlFor="requstNotes"
+                    className="text-gray-800 text-sm sm:text-base mb-2 pr-2 "
+                  >
+                    Notes:{" "}
+                  </label>
+                  <textarea
+                    name="requestNotes"
+                    id="requestNotes"
+                    value={requestNotes}
+                    onChange={(e) => setRequestNotes(e.target.value)}
+                    className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+                  />
                 </div>
                 <button
                   className="mt-4 w-full inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
