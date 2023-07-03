c7f6140b641d7d44cb89e45cc74778b5631d0977
CapSap <cshotam@gmail.com>
2023-07-03 12:53:22 +1000

    add some basic validation for ibt being numbers only

--- frontend/src/components/RequestCard.tsx
+++ frontend/src/components/RequestCard.tsx
@@ -153,6 +153,7 @@ export const RequestCard = ({
                     onChange={(e) => setIBT(e.target.value)}
                     className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
                   />
+                  {isNaN(ibt) ? <p>Please enter numbers</p> : null}
                 </div>
                 <button
                   className="mt-4 w-full inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
