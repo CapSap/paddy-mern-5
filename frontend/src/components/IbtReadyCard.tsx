426aa44be00c50f0f06703c9b1ff3c2b0568a348
CapSap <cshotam@gmail.com>
2023-08-05 19:06:14 +1000

    hide update button if no updates in ibt card

--- frontend/src/components/IbtReadyCard.tsx
+++ frontend/src/components/IbtReadyCard.tsx
@@ -121,12 +121,14 @@ export const IbtReadyCard = ({
                     checked={isIbtAccepeted}
                     onClick={() => setIsIbtAccepted(!isIbtAccepeted)}
                   />
-                  <button
-                    className="mt-4 w-full inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
-                    type="submit"
-                  >
-                    Update
-                  </button>
+                  {isIbtAccepeted ? (
+                    <button
+                      className="mt-4 w-full inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+                      type="submit"
+                    >
+                      Update
+                    </button>
+                  ) : null}
                 </form>
               </div>
             </div>
