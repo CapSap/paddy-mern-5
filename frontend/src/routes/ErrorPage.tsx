0e5f8c2eb01d9bf61ea416c14f1d63b667974065
CapSap <cshotam@gmail.com>
2023-08-09 23:27:48 +1000

    add styling for link

--- frontend/src/routes/ErrorPage.tsx
+++ frontend/src/routes/ErrorPage.tsx
@@ -9,6 +9,15 @@ export default function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
+        Report an issue{" "}
+        <a
+          href={"https://github.com/CapSap/paddy-mern-5/issues"}
+          className="underline text-blue-600 hover:text-blue-800"
+        >
+          here
+        </a>
+      </p>
+      <p>
         <i>
           {isRouteErrorResponse(error)
             ? error.statusText || error.error?.message
