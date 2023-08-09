443d9e03596a449ad11655f25465e305d9afcc79
CapSap <cshotam@gmail.com>
2023-08-09 23:37:36 +1000

    create basic navbar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+  return (
+    <div>
+      <a href="./">Store view</a>
+      <a href="./entry">Entry</a>
+      <a href="./ecom">Ecom View</a>
+    </div>
+  );
+};
