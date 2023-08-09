b9baf1a13890e86fec2bbbbc967fa428ebab504f
CapSap <cshotam@gmail.com>
2023-08-09 23:34:12 +1000

    add more styling to the error page

--- frontend/src/routes/ErrorPage.tsx
+++ frontend/src/routes/ErrorPage.tsx
@@ -5,10 +5,13 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div
+      id="error-page"
+      className="flex text-center justify-center flex-col mt-52"
+    >
+      <h1 className="text-5xl font-semibold mb-10">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+      <p className="pb-10">
         Report an issue{" "}
         <a
           href={"https://github.com/CapSap/paddy-mern-5/issues"}
