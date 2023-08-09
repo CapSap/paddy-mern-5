d4435e25a3d1797bda535e853832a93ff4f657f3
CapSap <cshotam@gmail.com>
2023-08-09 23:07:30 +1000

    type narrow unkown error type with isRouteErrorResponse

--- frontend/src/routes/ErrorPage.tsx
+++ frontend/src/routes/ErrorPage.tsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -9,7 +9,11 @@ export default function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>
+          {isRouteErrorResponse(error)
+            ? error.statusText || error.error?.message
+            : "unkown error message"}
+        </i>
       </p>
     </div>
   );
