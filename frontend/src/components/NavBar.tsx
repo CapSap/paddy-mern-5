85764e41e2637d4c68f724b301aaf03413c53fac
CapSap <cshotam@gmail.com>
2023-08-09 23:41:49 +1000

    use <Link/> from react-router instead of a tag

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const NavBar = () => {
   return (
     <div>
-      <a href="./">Store view</a>
-      <a href="./entry">Entry</a>
-      <a href="./ecom">Ecom View</a>
+      <Link to="/">Store view</Link>
+      <Link to="./entry">Entry</Link>
+      <Link to="./ecom">Ecom View</Link>
     </div>
   );
 };
